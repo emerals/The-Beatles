389370434b1547b3ca15486e6ab9a703cc8d7aaa
Emerson Almeida Souza <46051560+emerals@users.noreply.github.com>
2018-12-21 13:37:14 -0200

    Add files via upload

---
+++ Trabalho Beatles/Trabalho the beatles/Trabalho the beatles/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace Trabalho_the_beatles
+{
+    partial class Beatles
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_tempo_jonh = new System.Windows.Forms.TextBox();
+            this.tb_tempo_total = new System.Windows.Forms.TextBox();
+            this.tb_tempo_ringo = new System.Windows.Forms.TextBox();
+            this.tb_tempo_george = new System.Windows.Forms.TextBox();
+            this.tb_tempo_paul = new System.Windows.Forms.TextBox();
+            this.btn_Go = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tb_tempo_jonh
+            // 
+            this.tb_tempo_jonh.Location = new System.Drawing.Point(8, 23);
+            this.tb_tempo_jonh.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tb_tempo_jonh.Name = "tb_tempo_jonh";
+            this.tb_tempo_jonh.Size = new System.Drawing.Size(171, 20);
+            this.tb_tempo_jonh.TabIndex = 0;
+            this.tb_tempo_jonh.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.SomenteNumeros);
+            // 
+            // tb_tempo_total
+            // 
+            this.tb_tempo_total.Location = new System.Drawing.Point(8, 213);
+            this.tb_tempo_total.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tb_tempo_total.Name = "tb_tempo_total";
+            this.tb_tempo_total.Size = new System.Drawing.Size(171, 20);
+            this.tb_tempo_total.TabIndex = 2;
+            // 
+            // tb_tempo_ringo
+            // 
+            this.tb_tempo_ringo.Location = new System.Drawing.Point(8, 66);
+            this.tb_tempo_ringo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tb_tempo_ringo.Name = "tb_tempo_ringo";
+            this.tb_tempo_ringo.Size = new System.Drawing.Size(171, 20);
+            this.tb_tempo_ringo.TabIndex = 3;
+            this.tb_tempo_ringo.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.SomenteNumeros);
+            // 
+            // tb_tempo_george
+            // 
+            this.tb_tempo_george.Location = new System.Drawing.Point(8, 110);
+            this.tb_tempo_george.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tb_tempo_george.Name = "tb_tempo_george";
+            this.tb_tempo_george.Size = new System.Drawing.Size(171, 20);
+            this.tb_tempo_george.TabIndex = 4;
+            this.tb_tempo_george.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.SomenteNumeros);
+            // 
+            // tb_tempo_paul
+            // 
+            this.tb_tempo_paul.Location = new System.Drawing.Point(8, 155);
+            this.tb_tempo_paul.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tb_tempo_paul.Name = "tb_tempo_paul";
+            this.tb_tempo_paul.Size = new System.Drawing.Size(171, 20);
+            this.tb_tempo_paul.TabIndex = 5;
+            this.tb_tempo_paul.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.SomenteNumeros);
+            // 
+            // btn_Go
+            // 
+            this.btn_Go.Location = new System.Drawing.Point(8, 240);
+            this.btn_Go.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_Go.Name = "btn_Go";
+            this.btn_Go.Size = new System.Drawing.Size(169, 48);
+            this.btn_Go.TabIndex = 6;
+            this.btn_Go.Text = "GO";
+            this.btn_Go.UseVisualStyleBackColor = true;
+            this.btn_Go.Click += new System.EventHandler(this.btn_Go_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 8);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Jonh";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 140);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Paul";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(5, 96);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "George";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(8, 51);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Ringo";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(8, 192);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(119, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Tempo total em minutos";
+            // 
+            // Beatles
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Trabalho_the_beatles.Properties.Resources.the_beatles3_thumb_800x500_118857;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(421, 313);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_Go);
+            this.Controls.Add(this.tb_tempo_paul);
+            this.Controls.Add(this.tb_tempo_george);
+            this.Controls.Add(this.tb_tempo_ringo);
+            this.Controls.Add(this.tb_tempo_total);
+            this.Controls.Add(this.tb_tempo_jonh);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Beatles";
+            this.Text = "Beatles";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.SomenteNumeros);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_tempo_jonh;
+        private System.Windows.Forms.TextBox tb_tempo_total;
+        private System.Windows.Forms.TextBox tb_tempo_ringo;
+        private System.Windows.Forms.TextBox tb_tempo_george;
+        private System.Windows.Forms.TextBox tb_tempo_paul;
+        private System.Windows.Forms.Button btn_Go;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Trabalho Beatles/Trabalho the beatles/Trabalho the beatles/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Trabalho_the_beatles
+{
+    public partial class Beatles : Form
+    {
+        public Beatles()
+        {
+            InitializeComponent();
+            Inicializacao();
+        }
+
+        /*-----VARIÁVEIS GLOBAIS-----*/
+        int campos_repetidos = 0;
+        int TempoRingo = 0;
+        int TempoPaul = 0;
+        int TempoGeorge = 0;
+        int TempoJonh = 0;
+
+        /*-----METÓDOS-----*/
+        public void limpar()
+        {
+            tb_tempo_george.Text = "";
+            tb_tempo_jonh.Text = "";
+            tb_tempo_paul.Text = "";
+            tb_tempo_ringo.Text = "";
+            tb_tempo_jonh.Focus();//Primeiro campo do formulário
+        }
+  
+        public void Inicializacao()//Limitar os números de caracteres do form para dois digitos
+        {
+            tb_tempo_jonh.MaxLength = 2;
+            tb_tempo_george.MaxLength = 2;
+            tb_tempo_paul.MaxLength = 2;
+            tb_tempo_ringo.MaxLength = 2;
+
+            tb_tempo_total.Text = "17";/*Tempo total pré definido*/
+            tb_tempo_total.Enabled = true;/*Bloquear o tempo total para o usuário não digitar nada nele*/
+        }
+
+        public void TemposRepitidos()/*Método para caso algum tempo seja igual ao outro, assim daria conflito em quem seria o mais rápido*/
+        {           
+            TempoRingo = Convert.ToInt16(tb_tempo_ringo.Text);
+            TempoPaul = Convert.ToInt16(tb_tempo_paul.Text);
+            TempoGeorge = Convert.ToInt16(tb_tempo_george.Text);
+            TempoJonh = Convert.ToInt16(tb_tempo_jonh.Text);
+
+            if ((TempoRingo == TempoPaul) || (TempoRingo == TempoGeorge) || (TempoRingo == TempoJonh))//Inicio da verificação se tem campos com tempos iguais
+            {
+                campos_repetidos++;
+            }
+            if ((TempoPaul == TempoRingo) || (TempoPaul == TempoGeorge) || (TempoPaul == TempoJonh))
+            {
+                campos_repetidos++;
+            }
+            if ((TempoGeorge == TempoPaul) || (TempoGeorge == TempoRingo) || (TempoGeorge == TempoJonh))
+            {
+                campos_repetidos++;
+            }
+            if ((TempoJonh == TempoPaul) || (TempoJonh == TempoGeorge) || (TempoJonh == TempoRingo))//Fim da verificação de tempos iguais
+            {
+                campos_repetidos++;
+            }
+
+            int TempoTotal = Convert.ToInt16(tb_tempo_total.Text);//Tempo total de 17 minutos
+
+            if (campos_repetidos > 0)/*Se tiver algum campo repetido*/
+            {
+                MessageBox.Show("PREENCHA TODOS OS CAMPOS COM VELOCIDADES DIFERENTES!");
+                Application.Exit();
+            }/*Onde irá dar erro acaso o usuário digite um campo igual*/
+            
+        }
+
+        public void PassarPonteComMaisRapido()
+        {
+            TempoRingo = Convert.ToInt16(tb_tempo_ringo.Text);
+            TempoPaul = Convert.ToInt16(tb_tempo_paul.Text);
+            TempoGeorge = Convert.ToInt16(tb_tempo_george.Text);
+            TempoJonh = Convert.ToInt16(tb_tempo_jonh.Text);
+
+            int TempoTotal = Convert.ToInt16(tb_tempo_total.Text);//Tempo total de 17 minutos
+
+            int[] HereComesTheSun = new int[] {TempoRingo, TempoPaul, TempoGeorge, TempoJonh};
+            int[] YellowSubarine = HereComesTheSun;
+            int total = 0;
+            bool passou;
+
+            string BeattleMaisRapido = "";
+            string BeattleMaisRapido2 = "";
+            string BeattleMaisRapido3 = "";
+            string BeattleMaisRapido4 = "";
+
+            Array.Sort(HereComesTheSun);/*Ordenar array de forma crescente*/
+
+            for (int i = 0; i < 4; i++)//Somar o tempo total
+            {
+                if(i == 0) { total = HereComesTheSun[i] * 5; }/*Quem tá na posição 0 sempre será o mais rápido*/
+                if(i == 1) { total += HereComesTheSun[i]; }
+                if(i == 2) { total += HereComesTheSun[i]; }
+                if(i == 3) { total += HereComesTheSun[i]; }
+            }
+
+            int tempoTotal = Convert.ToInt16(tb_tempo_total.Text);
+
+                if (tempoTotal >= total)//Se o tempo de todos juntos for menor que o tempo total
+                {
+                    /*Calculo do tempo total para passagem*/
+
+                    MessageBox.Show("Os beatles estão atrevessando a ponte Rickety. Irão demorar"+" "+tempoTotal+" minutos para completar a travessia");
+                    passou = true;
+                }
+
+                else
+                {
+                    MessageBox.Show("ESTÃO MUITO DEVAGAR, NÃO CONSEGUIRÃO PASSAR A PONTE EM " +tempoTotal+" MINUTOS"+"..\n\n"
+                    +"TENTE IR MAIS RÁPIDO DESTA VEZ!!!"                    
+                    );
+                    passou = false;
+                    limpar();//Quando não conseguir passar eu limpo tudo
+                  
+                }
+               
+            if (passou == true)/*Se passaram, organiza como passarão*/
+                {
+                   if (TempoRingo == HereComesTheSun[0]){
+                    BeattleMaisRapido = "RINGO";
+                    BeattleMaisRapido2 = "GEORGE";
+                    BeattleMaisRapido3 = "PAUL";
+                    BeattleMaisRapido4 = "JOHN";
+                    }
+
+                    if (TempoGeorge == HereComesTheSun[0]){
+                     BeattleMaisRapido = "GEORGE";
+                     BeattleMaisRapido2 = "RINGO";
+                     BeattleMaisRapido3 = "PAUL";
+                     BeattleMaisRapido4 = "JOHN";
+                    }
+                    if (TempoJonh == HereComesTheSun[0]){
+                     BeattleMaisRapido = "JOHN";
+                    BeattleMaisRapido2 = "GEORGE";
+                    BeattleMaisRapido3 = "PAUL";
+                    BeattleMaisRapido4 = "RINGO";
+                    }
+                    if (TempoPaul == HereComesTheSun[0]){
+                    BeattleMaisRapido = "PAUL";
+                    BeattleMaisRapido2 = "GEORGE";
+                    BeattleMaisRapido3 = "RINGO";
+                    BeattleMaisRapido4 = "JOHN";
+                    }
+                    //Passando a ponte
+                    MessageBox.Show("PASSA " + BeattleMaisRapido + " (Com a Lanterna) E " + BeattleMaisRapido2);///Sempre o primeiro mais rápido irá passar
+                    MessageBox.Show("VOLTA " + BeattleMaisRapido + " (Com a Lanterna) ");
+                    MessageBox.Show("PASSA " + BeattleMaisRapido + " (Com a Lanterna) E " + BeattleMaisRapido3);
+                    MessageBox.Show("VOLTA " + BeattleMaisRapido + " (Com a Lanterna)");
+                    MessageBox.Show("PASSA " + BeattleMaisRapido + " (Com a Lanterna) E " + BeattleMaisRapido4);
+                    MessageBox.Show("TODOS OS BEATLES CONSIGUERAM CHEGAR NO SHOW A TEMPO!!!!");
+                    limpar();
+                    }                              
+        }
+        /*-----EVENTOS-----*/
+        private void SomenteNumeros(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsNumber(e.KeyChar) ||
+                e.KeyChar == (char)(Keys.Back))//Se for número e backspace
+            {
+                e.Handled = false;//Desligue o bloqueio   
+                if (e.KeyChar == (char)(Keys.Back))
+                { 
+                    
+                }
+            }
+            else
+            {
+                e.Handled = true;//Qualquer coisa diferente disso é bloqueado
+            }
+        }
+
+        private void btn_Go_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                TemposRepitidos(); /*Verificar se tem campo com valores iguais*/
+                PassarPonteComMaisRapido();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Todos os campos deverão ser preenchidos, digite novamente os tempos");
+                limpar();
+               
+            }
+        }
+
+
+    }
+}
